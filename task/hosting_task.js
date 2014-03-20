2da3fa4cdee9b29b0ee007df4708ca86847ed2a5
Christopher Gervais <chris@ergonlogic.com>
2014-03-20 20:46:46 +0100

    Move to the new AJAX mechanism for the hosting queue lists on nodes too.

--- task/hosting_task.js
+++ task/hosting_task.js
@@ -1,6 +1,7 @@
 (function($) {
 
-hostingTaskRefreshList = function() {
+hostingTaskRefreshList = function(latestVid, tasksOutstanding) {
+  // This function only applies to the node task list.
   if (!Drupal.settings.hostingTaskRefresh.nid) {
     return null;
   }
@@ -21,15 +22,40 @@ hostingTaskRefreshList = function() {
       }
     }
     else {
+      // Update the task list content.
       $("#hosting-task-list").html(data.markup);
 
+      // Allow buttons to open modal dialogs.
       hostingTaskBindButtons('#hosting-task-list');
-      setTimeout("hostingTaskRefreshList()", Drupal.settings.hostingTaskRefresh.refreshRate * 1000 );
+      // Trigger this parent function again re-using the current data.
+      setTimeout(function() { hostingTaskRefreshList(latestVid, statusChanged) }, Drupal.settings.hostingTaskRefresh.busyPollRate );
     }
   }
 
-  hostingTaskAddOverlay('#hosting-task-list');
-  $.get(Drupal.settings.basePath + 'hosting/tasks/' + Drupal.settings.hostingTaskRefresh.nid + '/list', null, hostingTaskListRefreshCallback , 'json' );
+  var hostingTaskListPollCallback = function(data) {
+    // Initilize a variable that will persist across calls to the parent function.
+    if (typeof(statusChanged) == 'undefined') { statusChanged = false; }
+    // Update the polling frequency based on the latest status.
+    pollTimeout = data.outstanding || statusChanged ? Drupal.settings.hostingTaskRefresh.busyPollRate : Drupal.settings.hostingTaskRefresh.idlePollRate;
+    // Keep track of when the status has changed across calls to the parent function.
+    statusChanged = data.outstanding != tasksOutstanding;
+    // Refresh the queue block whenever the latest status changes.
+    if (data.vid > latestVid) {
+      // Update the parameters
+      latestVid = data.vid;
+      tasksOutstanding = data.outstanding;
+      // Add throbber overlay to indicate that a refresh is in progress.
+      hostingTaskAddOverlay('#hosting-task-list');
+      // Request the updated queue block and pass the result to the callback for appropriate action.
+      $.get(Drupal.settings.basePath + 'hosting/tasks/' + Drupal.settings.hostingTaskRefresh.nid + '/list', null, hostingTaskListRefreshCallback , 'json' );
+    }
+    // Trigger the parent function again.
+    setTimeout(function() { hostingTaskRefreshList(latestVid, tasksOutstanding) }, pollTimeout );
+  }
+
+  // Poll the latest status and pass the result to the callback for appropriate action.
+  $.get(Drupal.settings.basePath + 'js/hosting_task/list_status', null, hostingTaskListPollCallback, 'json');
+
 }
 
 
@@ -51,7 +77,7 @@ hostingTaskRefreshQueueBlock = function(latestVid, tasksOutstanding) {
     // Allow buttons to open modal dialogs.
     hostingTaskBindButtons('#block-views-hosting-task-list-block');
     // Trigger this parent function again re-using the current data.
-    setTimeout(function() { hostingTaskRefreshQueueBlock(latestVid, statusChanged) }, 1000 );
+    setTimeout(function() { hostingTaskRefreshQueueBlock(latestVid, statusChanged) }, Drupal.settings.hostingTaskRefresh.busyPollRate );
   }
 
   var hostingTaskQueuePollCallback = function(data) {
@@ -87,7 +113,7 @@ $(document).ready(function() {
   var tasksOutstanding = Drupal.settings.hostingTaskRefresh.tasksOutstanding;
   // Set fast polling when outstanding tasks exist, otherwise use our configured refresh rate.
   pollTimeout = tasksOutstanding ? Drupal.settings.hostingTaskRefresh.busyPollRate : Drupal.settings.hostingTaskRefresh.idlePollRate;
-  setTimeout("hostingTaskRefreshList()", pollTimeout );
+  setTimeout(function() { hostingTaskRefreshList(latestVid, tasksOutstanding) }, pollTimeout );
   setTimeout(function() { hostingTaskRefreshQueueBlock(latestVid, tasksOutstanding) }, pollTimeout );
   hostingTaskBindButtons($(this));
   $('#hosting-task-confirm-form-actions a').click(function() {
